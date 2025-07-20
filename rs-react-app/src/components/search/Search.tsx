1c75b8d1b1b4e7a0802f1d76ac4fbb07141913f2
Aliaksandr Iluishenka <sasha29292931@gmail.com>
2025-07-20 05:18:54 +0300

    feat: split code into separated parts

---
+++ rs-react-app/src/components/search/Search.tsx
@@ -0,0 +1,39 @@
+import { Component, type ChangeEvent, type FormEvent } from 'react';
+import styles from './search.module.css';
+
+type SearchProps = {
+  onSearchChange: (value: string) => void;
+  onSearchSubmit: () => Promise<void>;
+  searchValue: string;
+};
+
+class Search extends Component<SearchProps> {
+  onSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target instanceof HTMLInputElement) {
+      const searchValue = e.target.value;
+      this.props.onSearchChange(searchValue);
+    }
+  };
+
+  onSearchSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    if (e.target instanceof HTMLFormElement) {
+      e.preventDefault();
+      this.props.onSearchSubmit();
+    }
+  };
+
+  render() {
+    return (
+      <form className={styles.topControls} onSubmit={this.onSearchSubmit}>
+        <input
+          type="text"
+          onChange={this.onSearchChange}
+          value={this.props.searchValue}
+        />
+        <button>Search</button>
+      </form>
+    );
+  }
+}
+
+export default Search;
