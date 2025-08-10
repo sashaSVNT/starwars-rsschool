4d98d5a53a00a7627fa5b777b6f22964de047ee8
Aliaksandr Iluishenka <sasha29292931@gmail.com>
2025-08-10 20:43:27 +0300

    feat: implement RTK Query to make api calls through hook useGetPersonByIdQuery and refactor PersonDetails using this hook

--- rs-react-app/src/types/apiResponse.type.ts
+++ rs-react-app/src/types/apiResponse.type.ts
@@ -1,18 +1,5 @@
 import type { PersonResult } from './personResult.type';
 
-export type GetAllPeopleResponse = {
-  message: string;
-  next: string;
-  previous: string;
-  results: PersonResult[];
-  total_pages: number;
-};
-
-export type GetPeopleByQueryResponse = {
-  message: string;
-  result: PersonResult[];
-};
-
 export type GetPersonByIdResponse = {
   message: string;
   result: PersonResult;
