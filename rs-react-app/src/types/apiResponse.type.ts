daff6b9704e6789e339fc25b57a1c59ea9226703
Aliaksandr Iluishenka <sasha29292931@gmail.com>
2025-08-10 22:21:05 +0300

    feat: add new implementation to get people by RTK hook

--- rs-react-app/src/types/apiResponse.type.ts
+++ rs-react-app/src/types/apiResponse.type.ts
@@ -5,6 +5,17 @@ export type GetPersonByIdResponse = {
   result: PersonResult;
 };
 
+export type GetPeopleBySearchWord = {
+  message: string;
+  total_pages: number;
+  results: PersonResult[];
+};
+
+export type GetPeopleByLimit = {
+  message: string;
+  result: PersonResult[];
+};
+
 export type GetPeopleResponse = {
   results: PersonResult[];
   totalPages: number;
